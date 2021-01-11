6ef05724f6813dc4f80417c48ddf7c87bc0eab78
AnotherFraud <67690136+AnotherFraud@users.noreply.github.com>
2021-01-11 20:10:45 +0100

    Add files via upload

---
+++ WriteToExcelTemplateXLSX/src/org/AnotherFraudExcel/WriteToExcelTemplateXLSXNodeView.java
@@ -0,0 +1,76 @@
+package org.AnotherFraudExcel;
+
+import org.knime.core.node.NodeView;
+
+
+/*
+ * Continental Nodes for KNIME
+ * Copyright (C) 2019  Continental AG, Hanover, Germany
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * @author Another Fraud
+ */
+
+
+public class WriteToExcelTemplateXLSXNodeView extends NodeView<WriteToExcelTemplateXLSXNodeModel> {
+
+    /**
+     * Creates a new view.
+     * 
+     * @param nodeModel The model (class: {@link WriteToExcelTemplateXLSXNodeModel})
+     */
+    protected WriteToExcelTemplateXLSXNodeView(final WriteToExcelTemplateXLSXNodeModel nodeModel) {
+        super(nodeModel);
+
+        // TODO instantiate the components of the view here.
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void modelChanged() {
+
+        // TODO retrieve the new model from your nodemodel and 
+        // update the view.
+        WriteToExcelTemplateXLSXNodeModel nodeModel = 
+            (WriteToExcelTemplateXLSXNodeModel)getNodeModel();
+        assert nodeModel != null;
+        
+        // be aware of a possibly not executed nodeModel! The data you retrieve
+        // from your nodemodel could be null, emtpy, or invalid in any kind.
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void onClose() {
+    
+        // TODO things to do when closing the view
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void onOpen() {
+
+        // TODO things to do when opening the view
+    }
+
+}
+
